0c15e104b3a8eb150a2e18edc0c25d8ed015cfe7
Charlie Dil <charliedil2019@gmail.com>
2025-03-25 15:10:17 -0400

    added inference

--- model.py
+++ model.py
@@ -41,7 +41,8 @@ class DeBertaModel(L.LightningModule): #added inheritance to lightning module he
             self.log('train_loss', loss)
             print(loss)
             return loss
-
+        def predict_step(self, batch , batch_idx, dataloader_idx=0):
+            return self(batch["input_ids"], batch["attention_mask"])
 
         #Optimizers
         def configure_optimizers(self):
@@ -50,7 +51,7 @@ class DeBertaModel(L.LightningModule): #added inheritance to lightning module he
 
 
 #Data Preperation Class
-class TextDataset(Dataset):
+class TrainDataset(Dataset):
 
     def __init__(self, texts, labels, tokenizer, max_len):
 
@@ -105,6 +106,56 @@ class TextDataset(Dataset):
             'labels': torch.tensor(label, dtype=torch.long)
 
         }
+class TestDataset(Dataset):
+
+    def __init__(self, texts, tokenizer, max_len):
+
+        self.texts = texts #sentences
+
+        self.tokenizer = tokenizer
+
+        self.max_len = max_len
+
+
+
+    def __len__(self):
+
+        return len(self.texts)
+
+
+
+    def __getitem__(self, idx):
+
+        text = self.texts[idx]
+
+        encoding = self.tokenizer.encode_plus(
+
+            text,
+
+            add_special_tokens=True,
+
+            max_length=self.max_len,
+
+            return_token_type_ids=False,
+
+            padding='max_length',
+
+            return_attention_mask=True,
+
+            return_tensors='pt',
+
+            truncation=True,
+
+        )
+
+        return {
+
+            'input_ids': encoding['input_ids'].flatten(),
+
+            'attention_mask': encoding['attention_mask'].flatten(),
+
+        }
+
 def map_labels(labels): #map labels to ints (model works with numbers, not string labels :) )
     label_to_int = {}
     counter = 1
@@ -126,7 +177,8 @@ def get_max_len_sent(tokenizer, sents):
 # Prepare data
 
 # Open and read the JSON file
-
+load_checkpoint = True
+checkpoint = 'lightning_logs/version_52/checkpoints/epoch=0-step=2802.ckpt'
 train_texts = []
 train_labels = []
 
@@ -148,24 +200,27 @@ for item in traindata:
 with open(testfile, "r") as file:
     testdata = json.load(file)
 for item in testdata:
-    test_texts.append(item["sample"]
+    test_texts.append(item["sample"])
 label_to_int = map_labels(train_labels)
 
 print(label_to_int)
 
 train_labels = [label_to_int[label] for label in train_labels] # all this is doing is turning the labels into their respective int
 tokenizer = DebertaV2Tokenizer.from_pretrained("microsoft/deberta-v3-base", use_fast=False)
-max_len = get_max_len_sent(tokenizer, train_texts)
-train_dataset = TextDataset(train_texts, train_labels, tokenizer = tokenizer,max_len=max_len)
+max_len = get_max_len_sent(tokenizer, train_texts)+50 #just some leeway
+train_dataset = TrainDataset(train_texts, train_labels, tokenizer = tokenizer,max_len=max_len)
+test_dataset = TestDataset(test_texts, tokenizer=tokenizer, max_len=max_len)
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
 # Initialize model
-
 model = DeBertaModel()
-
-# Train model
-
-trainer = Trainer(max_epochs=3, accelerator="gpu", precision="bf16-mixed") #TODO: keep precision, maybe increase GPUs if other two changes don't work out
-
-trainer.fit(model, train_loader)
+trainer = Trainer(max_epochs=1, accelerator="gpu", precision="bf16-mixed") #TODO: keep precision, maybe increase GPUs if other two changes don't work out
+if load_checkpoint:
+    model = DeBertaModel.load_from_checkpoint(checkpoint)
+    trainer = Trainer()
+else:
+    trainer.fit(model, train_loader)
+predictions = trainer.predict(model, test_loader)
+print(predictions)
