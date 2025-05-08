65676c17d340c07c17d136e97d408d4796792726
charliedil <charliedil2019@gmail.com>
2025-05-08 05:12:30 -0400

    fixed preprocessing... again, edited gitignore to exclude certain folders

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=DebertaRE
-#SBATCH --gres=gpu:40g:1 
-#SBATCH --cpus-per-task=6
+#SBATCH --job-name=mixedpredict
+#SBATCH --gpus=1 
+#SBATCH --cpus-per-task=4
 #SBATCH --output=output.log
 #SBATCH --time=14-00:00
 #SBATCH --mem=100G
@@ -11,15 +11,14 @@ module load python/3.11
 
 python -m venv venv
 source venv/bin/activate
-rm checkpoints/*
 pip install -r requirements.txt
-python preprocessing.py --test_in ../GutBrainIE_Full_Collection_2025/Annotations/Dev/json_format/dev.json --test_out testData.json
-#python preprocessing.py --test_in ../gliner_preds_fixed_fancy.json --test_out testData.json
-python preprocessing.py --train_in ../GutBrainIE_Full_Collection_2025/Annotations/Train/platinum_quality/json_format/train_platinum.json --train_out trainPlatinum.json --val_out valPlatinum.json
+#python preprocessing.py --test_in ../GutBrainIE_Full_Collection_2025/Annotations/Dev/json_format/dev.json --test_out testData.json
+python preprocessing.py --test_in ../ensemble_1_preds_with_rules.json --test_out testData.json
+#python preprocessing.py --train_in ../GutBrainIE_Full_Collection_2025/Annotations/Train/platinum_quality/json_format/train_platinum.json --train_out trainPlatinum.json --val_out valPlatinum.json
 #python preprocessing.py --train_in ../GutBrainIE_Full_Collection_2025/Annotations/Train/gold_quality/json_format/train_gold.json --train_out trainGold.json --val_out valGold.json
 #python preprocessing.py --train_in ../GutBrainIE_Full_Collection_2025/Annotations/Train/silver_quality/json_format/train_silver.json --train_out trainSilver.json --val_out valSilver.json
-python combineJSONFiles.py trainPlatinum.json trainData.json
-python combineJSONFiles.py valPlatinum.json valData.json
-python main.py fit --config=my_config.yaml 
-python main.py predict --config=my_config.yaml --ckpt_path checkpoints/best-checkpoint.ckpt
+#python combineJSONFiles.py trainPlatinum.json trainData.json
+#python combineJSONFiles.py valPlatinum.json valData.json
+#python main.py fit --config=my_config.yaml 
+python main.py predict --config=my_config.yaml --ckpt_path checkpoints/mixed_cnn_full.ckpt
 python postprocessing.py predictions.pkl testData.json
